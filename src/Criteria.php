9566224449ed68cc00a67fa8d60b145d0de1ebcb
Blue <757470062@qq.com>
2017-12-20 06:50:44 +0800

    Update Criteria.php

--- src/Criteria.php
+++ src/Criteria.php
@@ -21,34 +21,44 @@ trait Criteria
 
         $model = $model->where(function ($query) use ($request, $fieldSearchable, $isFirstField){
             foreach ($fieldSearchable as $field => $condition){
+
                 if (!is_null($request[$field])){
                     $value = $request[$field];
+
+                    if ((strcasecmp($condition[0], '=') !== 0 && strcasecmp($condition[0], 'like') !== 0) && !is_array($value)){
+                        throw new \Exception(__METHOD__.':'.'use [ '.$condition[0].' ] select field. So [ '.$field.' ] must be array()');
+                    }
 
                     if (strcasecmp($condition[0], 'like') === 0){
                         $value = '%'. $request[$field] .'%';
                     }
 
-                    if ($isFirstField || strcasecmp($condition[1], 'and') === 0){
-                        $query->where($field, $condition[0], $value);
-                        $isFirstField = false;
-                    }else{
-                        if (strcasecmp($condition[1], 'between') === 0){
+                    if (strcasecmp($condition[1], 'and') === 0){
+                        if($isFirstField || strcasecmp($condition[0], '=') === 0 || strcasecmp($condition[0], 'like') === 0){
+                            $query->where($field, $condition[0], $value);
+                            $isFirstField = false;
+                        }else if (strcasecmp($condition[0], 'between') === 0){
                             $query->whereBetween($field, $value);
-                        }
-                        if (strcasecmp($condition[1], 'notBetween') === 0){
+                        }else if (strcasecmp($condition[0], 'notBetween') === 0){
                             $query->whereNotBetween($field, $value);
-                        }
-                        if (strcasecmp($condition[1], 'in') === 0){
+                        }else if (strcasecmp($condition[0], 'in') === 0){
                             $query->whereIn($field, $value);
+                        }else if (strcasecmp($condition[0], 'notIn') === 0){
+                            $query->whereNotIn($field, [$value]);
                         }
-                        if (strcasecmp($condition[1], 'notIn') === 0){
-                            $query->whereNotIn($field, $value);
-                        }
-                        if(strcasecmp($condition[1], 'or') === 0){
+                    }else{
+                        if(strcasecmp($condition[0], '=') === 0 || strcasecmp($condition[0], 'like') === 0){
                             $query->orWhere($field, $condition[0], $value);
+                        }else if (strcasecmp($condition[0], 'between') === 0){
+                            $query->orWhereBetween($field, $value);
+                        }else if (strcasecmp($condition[0], 'notBetween') === 0){
+                            $query->orWhereNotBetween($field, $value);
+                        }else if (strcasecmp($condition[0], 'in') === 0){
+                            $query->orWhereIn($field, [$value]);
+                        }else if (strcasecmp($condition[0], 'notIn') === 0){
+                            $query->orWhereNotIn($field, [$value]);
                         }
                     }
-
                 }
             }
 
